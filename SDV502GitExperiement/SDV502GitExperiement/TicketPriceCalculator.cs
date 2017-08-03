419fa08abb4e01423734580f9e9dfa065f5a1e9b
DiODonnell <30705361+DiODonnell@users.noreply.github.com>
2017-08-04 10:13:09 +1200

    Added a calculate ticket class

---
+++ SDV502GitExperiement/SDV502GitExperiement/TicketPriceCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDV502GitExperiement
+{
+    public class TicketPriceCalculator
+    {
+    }
+}
